10b4c7f8a988517a2aaaa91a9fbada74dbcdac1c
JiYoung <yj0zero@gmail.com>
2022-08-01 18:00:42 +0900

    interface의 상속
    TS interface의 상속
    
    TS의 기능 중 interface의 용도와 상속 방법에 대해 공부하였습니다.

---
+++ TypeScript/interface.js
@@ -0,0 +1,26 @@
+interface User {
+	firstName:string,
+	lastName:string,
+	sayHi(name: string):string
+	fullName(): string
+}
+
+interface Human {
+	health: number
+}
+
+class Player implements User, Human { 
+	constructor(
+    	public firstName: string,
+        public lastName: string.
+        public health: number
+    ){}
+    fullName(){
+    	return `${this.firstName} ${this.lastName}`
+    }
+    
+    sayHi(name: string){
+    	return `Hello ${name}. My name is ${this.fullName()}`
+    } 
+    
+}
